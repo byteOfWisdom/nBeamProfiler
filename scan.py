8bd1f31a9ad85114001ed6b4a24355f17e4d913b
Leoni <leoni.dessau@mailbox.org>
2025-09-15 12:18:46 +0200

    added sync pulse

--- scan.py
+++ scan.py
@@ -1,28 +1,48 @@
 from zaber_motion import Units
 from zaber_motion.ascii import Connection
+import RPi.GPIO as GPIO
+from sys import argv
 
 max_speed = 10.
+scan_speed = 10.
 
 def make_scan_path(xsize, ysize, line_count):
     command_list = []
     command_list.append((0, 0, max_speed))
 
-    ...
+    fwd = True
+    for i in range(line_count):
+        command_list.append((xsize if fwd else 0, (ysize / line_count) * i, scan_speed)) # the actual scan line
+        command_list.append(((xsize if fwd else 0, (ysize / line_count) * (i + 1), max_speed))) # the height diff
 
-with Connection.open_serial_port("COM3") as connection:
-    connection.enable_alerts()
+    return command_list[:-1] # don't need the last vertical step
 
-    device_list = connection.detect_devices()
-    print("Found {} devices".format(len(device_list)))
 
-    device = device_list[0]
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(sync_pin, GPIO.OUT)
+    # requies argv: serialport, linecount, xsize (in cm), ysize (in cm)
 
-    axis = device.get_axis(1)
-    if not axis.is_homed():
-      axis.home()
+    scan_cmds = make_scan_path(float(argv[3]) if len(argv) > 3 else 30., float(argv[4]) if len(argv) > 4 else 30., int(argv[2]) if len(argv) > 2 else 20)
 
-    # Move to 10mm
-    axis.move_absolute(10, Units.LENGTH_MILLIMETRES)
+    with Connection.open_serial_port(argv[1]) as connection:
+        connection.enable_alerts()
 
-    # Move by an additional 5mm
-    axis.move_relative(5, Units.LENGTH_MILLIMETRES)
+        device_list = connection.detect_devices()
+        print("Found {} devices".format(len(device_list)))
+
+        device = device_list[0]
+
+        xaxis = device.get_axis(1)
+        if not xaxis.is_homed():
+            xaxis.home()
+
+        yaxis = device.get_axis(2)
+        if not yaxis.is_homed():
+            yaxis.home()
+
+        for x, y, s in scan_cmds:
+            GPIO.output(sync_pin, GPIO.HIGH)
+            xaxis.move_absolute(x * 10., Units.LENGTH_MILLIMETRES, s)
+            GPIO.output(sync_pin, GPIO.LOW)
+            yaxis.move_absolute(y * 10., Units.LENGTH_MILLIMETRES, s)
