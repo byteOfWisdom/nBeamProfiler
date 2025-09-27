8854945e7017b5d2df6a9265694e8fd5786a520d
Leoni <leoni.dessau@mailbox.org>
2025-09-27 14:42:06 +0200

    adkj

---
+++ deconv.jl
@@ -0,0 +1,59 @@
+using Base: absdiff
+using FFTW
+using CSV
+using DeconvOptim # doi:10.21105/jcon.00099
+using Plots
+using Images
+
+
+function load_grid(fname)
+    print(fname)
+    cols(a) = [view(a, :, i) for i in 1:size(a, 2)]
+    data = cols(CSV.read(ARGS[1], CSV.Tables.matrix; delim=", "))
+
+    xbins = data[1]
+    ybins = data[2]
+    values = data[3]
+
+    nrows = maximum(xbins)
+    ncols = maximum(ybins)
+
+    num_grid = [[0.0 for ny in range(1, ncols + 1)] for nx in range(1, nrows + 1)]
+
+    for i in eachindex(xbins)
+        x = trunc(Int, xbins[i])
+        y = trunc(Int, ybins[i])
+        num_grid[x + 1][y + 1] = values[i]
+    end
+
+
+    num_grid = reduce(hcat, num_grid)'
+    return num_grid
+end
+
+function main()
+    iterations = 1000
+    iterations = parse(Int64, ARGS[3])
+
+    num_grid = load_grid(ARGS[1])
+    scint = load_grid(ARGS[2])
+
+    beam = deconvolution(num_grid, scint, regularizer=Tikhonov(), iterations=iterations)[1]
+
+    total_diff = sum((num_grid .- (conv(beam, scint) ./ maximum(conv(beam, scint)))) .^ 2)
+    print("total_diff = ")
+    println(total_diff)
+
+    h1 = heatmap(num_grid, title="measured")
+    h2 = heatmap(num_grid .- (conv(beam, scint) ./ maximum(conv(beam, scint))), title="difference")
+    h3 = heatmap(beam, title="calculated beam")
+    # h4 = heatmap(conv(beam, scint)./ maximum(conv(beam, scint)), title="reconvoluted")
+    h4 = heatmap(scint, title="scintillator")
+    plot(h1, h2, h3, h4, layout=(2, 2))
+
+
+    gui()
+end
+
+
+main()
---
+++ square_scint.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+
+def csv_print(*cols):
+    for i in range(len(cols[0])):
+        print(", ".join([str(c[i]) for c in cols]))
+        
+
+
+if __name__ == "__main__":
+    _ = print("args: [bin count] [size] [scan area size]") if len(argv) < 3 else None
+    size = float(argv[3]) / float(argv[2])
+    bin_count = int(argv[1])
+
+    grid = [[0.0 for _ in range(bin_count)] for _ in range(bin_count)]
+
+    high_bins = size * 100. / bin_count 
+
+    low_cut = int(high_bins // 2)
+    high_cut =bin_count - low_cut
+
+    xs = []
+    ys = []
+    amps = []
+    for i in range(bin_count):
+        for j in range(bin_count):
+            xs.append(i)
+            ys.append(j)
+            amps.append(1.0 if (i < low_cut or i > high_cut) and (j < low_cut or j > high_cut) else 0.0)
+
+    csv_print(xs, ys, amps)
