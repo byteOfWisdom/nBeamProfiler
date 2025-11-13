2f39fa3390eec642d11dec2352f285e8ca21db09
Leoni <leoni.dessau@mailbox.org>
2025-11-13 09:18:22 +0100

    better julia api call

--- py_ppl/deconv.py
+++ py_ppl/deconv.py
@@ -4,4 +4,5 @@ jl.seval("using DeconvOptim")
 
 
 def deconv(measured, scint, iterations):
-    return jl.deconvolution(measured, scint, regularizer=jl.Tikhonov(), iterations=iterations)
+    res, meta = jl.deconvolution(measured, scint, regularizer=jl.Tikhonov(), iterations=iterations)
+    return res, meta
