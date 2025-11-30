b83319a0f5c362f9b635c698f0205c0e5fae6b74
Leoni <leoni.dessau@mailbox.org>
2025-11-30 21:22:18 +0100

    removed julia dependency

--- py_ppl/deconv.py
+++ py_ppl/deconv.py
@@ -1,14 +1,14 @@
-from juliacall import Main as jl
-import numpy as np
+# from juliacall import Main as jl
+# import numpy as np
 from skimage import restoration
 
-jl.seval("using DeconvOptim")
+# jl.seval("using DeconvOptim")
 
 
-def deconv(measured, scint, iterations):
-    # res, meta = jl.deconvolution(measured, scint, regularizer=jl.TV(), iterations=iterations)
-    res, meta = jl.deconvolution(measured, scint, iterations=iterations, loss=jl.Gauss())
-    return np.array(res), meta
+# def deconv(measured, scint, iterations):
+#     res, meta = jl.deconvolution(measured, scint, regularizer=jl.TV(), iterations=iterations)
+#     res, meta = jl.deconvolution(measured, scint, iterations=iterations, loss=jl.Gauss())
+#     return np.array(res), meta
 
 
 def deconv_rl(measured, scint, iterations):
