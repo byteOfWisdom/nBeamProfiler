72db321ee0e9b4256289592a2786344e04d4b2d1
Leoni <leoni.dessau@mailbox.org>
2025-11-30 17:16:15 +0100

    asdf

--- py_ppl/sim_data.py
+++ py_ppl/sim_data.py
@@ -6,14 +6,19 @@ from sys import argv
 
 
 def beam_func(x, y):
-    return np.exp(- ((x ** 2 + y ** 2) / 6) ** 4)
+    return np.exp(-(((x**2 + y**2) / 6) ** 4))
+
+
+def absmin(a, b):
+    return a if np.abs(a) < np.abs(b) else b
 
 
 def time_point_gen(dt):
     t = 0
     speed = 5
+    vert_step = 30.0 / 160
     x, y, t = 0, 0, 0
-    to_x, to_y = 0., 0
+    to_x, to_y = 0.0, 0
     current_line = 0
     time_pulse = False
     fwd = True
@@ -22,15 +27,30 @@ def time_point_gen(dt):
         mx, my = to_x - x, to_y - y
         t += dt
         if mx:
-            x += min(mx, speed * dt * (mx / np.abs(mx)))
+            x += absmin(mx, speed * dt * (mx / np.abs(mx)))
         elif my:
-            y += min(my, speed * dt * (my / np.abs(my)))
+            y += absmin(my, speed * dt * (my / np.abs(my)))
         else:
             time_pulse = True
             if fwd and horizontal:
-                
+                fwd = not fwd
+                horizontal = False
+                to_x = 30.0
+            elif not fwd and horizontal:
+                fwd = not fwd
+                horizontal = False
+                to_x = 0.0
+            elif not horizontal:
+                print(f"finished line {current_line}")
+                horizontal = True
+                current_line += 1
+                to_y += vert_step
+
         yield t, x, y, time_pulse
         time_pulse = False
+
+        if current_line > 160:
+            return None
 
 
 sigma_gamma_sq = 0.0007910551062305672
@@ -42,38 +62,50 @@ mu_neutron = 0.44286772376157973
 
 
 def main():
-    xs, ys = np.meshgrid(np.linspace(0, 30., 160), np.linspace(0, 30., 160))
-    # beam = beam_func(xs - 15., ys - 15.)
+    xs, ys = np.meshgrid(np.linspace(0, 30.0, 160), np.linspace(0, 30.0, 160))
+    beam = beam_func(xs, ys)
 
     rng = np.random.default_rng()
 
     raw_scint = square_scint.square_scint(160, 2.54)
     scint = np.zeros((160, 160))
-    i, j = np.meshgrid(np.arange(np.shape(raw_scint)[1]), np.arange(np.shape(raw_scint)[0]))
+    i, j = np.meshgrid(
+        np.arange(np.shape(raw_scint)[1]), np.arange(np.shape(raw_scint)[0])
+    )
     scint[i, j] += raw_scint[i, j]
+
+    scint = np.roll(scint, 80, 0)
+    scint = np.roll(scint, 80, 1)
     time_const = 1 / 6.25e-8
 
-    ax = plt.figure().add_subplot(projection='3d')
-    ax.contour(xs, ys, scint, levels=100)
+    ax = plt.figure().add_subplot(projection="3d")
+    ax.contour(xs, ys, scint, levels=100, cmap=plt.cm.viridis)
+    ax.contour(xs, ys, beam, levels=100, cmap=plt.cm.inferno)
     plt.show()
 
-    dt = 0.01
+    dt = 0.05
 
     file = open(argv[1], "w")
+
+    total_hits = 0
 
     for t, x, y, tp in time_point_gen(dt):
         if tp:
             file.write(f"{5}, {1}, {int(t * time_const)}, {3}\n")
         hit_count = int(1e3 * np.sum(beam_func(xs - x, ys - y) * scint))
         # print(hit_count)
+        total_hits += hit_count
         neutron_ys = iter(rng.normal(mu_neutron, sigma_neutron, hit_count))
         gamma_ys = iter(rng.normal(mu_gamma, sigma_gamma, hit_count))
         for tc in time_const * np.linspace(t, t + dt, num=hit_count, endpoint=False):
             long = 1000
-            file.write(f"{long}, {int(long * (1 - next(neutron_ys)))}, {int(tc)}, {2}\n")
+            file.write(
+                f"{long}, {int(long * (1 - next(neutron_ys)))}, {int(tc)}, {2}\n"
+            )
             file.write(f"{long}, {int(long * (1 - next(gamma_ys)))}, {int(tc)}, {2}\n")
 
     file.close()
+    print(f"total neutron hit count is {total_hits}")
 
 
 if __name__ == "__main__":
