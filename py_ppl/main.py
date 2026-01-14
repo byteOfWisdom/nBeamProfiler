97da8474883aab82cd17ef800548207a158f0990
MaxLoep <Loepke@uni-bonn.de>
2026-01-14 02:36:41 +0100

    2D-supergaussian now fitting because it is elliptical

--- py_ppl/main.py
+++ py_ppl/main.py
@@ -62,14 +62,15 @@ def matrix(arr):
     return res
 
 # Define the 2D super-Gaussian model
-def super_gaussian_2d(coords, A, x0, y0, sigma_x, sigma_y, n):
+def super_gaussian_2d(coords, A, x0, y0, sigma_x, sigma_y, n, offset):
     x, y = coords
     # Ensure sigma values and exponent are positive
     sigma_x = abs(sigma_x)
     sigma_y = abs(sigma_y)
     n = abs(n)
-    exponent = ((x - x0) / sigma_x) ** (2 * n) + ((y - y0) / sigma_y) ** (2 * n)
-    return (A * np.exp(-exponent) ).ravel()
+    # exponent = -((x - x0) / sigma_x) ** (2*n) - ((y - y0) / sigma_y) ** (2*n)              #rectangular gaussian beam 
+    exponent = -( ( (x - x0)/(2*sigma_x) )  **2   + ( (y - y0)/(2*sigma_y) )**2 ) ** n       #elliptical gaussian beam, thanks wikipedia :)
+    return (A * np.exp(exponent) + offset).ravel()
 
 
 
@@ -194,44 +195,15 @@ def main():
         ax.title.set_text("unfolded data")
         ax.view_init(elev=45, azim=-45, roll=0)
         x, y = np.meshgrid(np.linspace(0,30,np.max(data[0]) + 1), np.linspace(0,30,np.max(data[1]) + 1))#meshgrid with lines to 30cm
-        # ax.contour(x, y, result, levels=100, axlim_clip=True)
-        # ax.contourf(x, y, result, zdir='x', offset=lower_x, levels=300, cmap='rainbow', axlim_clip=True)
-        # ax.contourf(x, y, result, zdir='y', offset=upper_y, levels=300, cmap='rainbow', axlim_clip=True)               #projection with scaled to 30cm
-
-        #Test Data for 2D Super Gaus
-        true_params = [1, 16, 16, 2.0, 2.0, 2]  # A, x0, y0, sigma_x, sigma_y, n, offset
-        Z_true = super_gaussian_2d((x, y), *true_params).reshape(np.max(data[1]) + 1, np.max(data[1]) + 1)
-        ax.contour(x, y, Z_true, levels=300, axlim_clip=True)
-        ax.contourf(x, y, Z_true, zdir='x', offset=lower_x, levels=300, cmap='rainbow', axlim_clip=True)
-        ax.contourf(x, y, Z_true, zdir='y', offset=upper_y, levels=300, cmap='rainbow', axlim_clip=True)
-
-
-
-        # popt, pcov = curve_fit(
-        #                         super_gaussian_2d
-        #                         ,(x, y)
-        #                         ,result.ravel()
-        #                         ,p0=[1, 16, 16, 5, 5, 1]         # initial_guess
-        #                         ,maxfev=99999999
-        #                         ,bounds=(
-        #                                 [0, 0, 0, 0.1, 0.1, 1],  # lower bounds
-        #                                 [1, 30, 30, 500, 500, 10]    # upper bounds
-        #                                 )
-        #                         )
-        # # Extract fitted parameters
-        # A_fit, x0_fit, y0_fit, sigma_x_fit, sigma_y_fit, n_fit, offset_fit = popt
-        # print("Fitted parameters:")
-        # print(f"A={A_fit:.3f}, x0={x0_fit:.3f}, y0={y0_fit:.3f}, "
-        # f"sigma_x={sigma_x_fit:.3f}, sigma_y={sigma_y_fit:.3f}, "
-        # f"n={n_fit:.3f}, offset={offset_fit:.3f}")
-
+        ax.contour(x, y, result, levels=100, axlim_clip=True)
+        ax.contourf(x, y, result, zdir='x', offset=lower_x, levels=300, cmap='rainbow', axlim_clip=True)
+        ax.contourf(x, y, result, zdir='y', offset=upper_y, levels=300, cmap='rainbow', axlim_clip=True)               #projection with scaled to 30cm
         ax.set_zlim(0,1.1)
         ax.set_xlim(lower_x,upper_x)
         ax.set_ylim(lower_y,upper_y)
         ax.set_xlabel("x / cm")
         ax.set_ylabel("y / cm")
         ax.set_zlabel("normalised intensity")
-       
 
         #RE-FOLDED DATA as contour plot
         ax = fig.add_subplot(2, 2, 3, projection='3d')
@@ -248,25 +220,70 @@ def main():
         ax.set_ylabel("y / cm")
         ax.set_zlabel("normalised intensity")
 
-        #STOPPING CRITERIA
-        ax = fig.add_subplot(2, 2, 4)
-        ax.title.set_text("change in unfolding")
-        x = np.linspace(1,args["iterations"], num=args["iterations"])
-        ax.plot(x,diff1, color='r', label='change between iterations')
-        ax.plot(x,diff2, color='blue', label='diff between re-convoluted and raw data')
-        # ax.set_xlim(0,30)
-        # ax.set_ylim(0,30)
-        # ax.set_xlim(lower_x,upper_x)
-        # ax.set_ylim(lower_y,upper_y)
-        ax.grid()
-        ax.set_yscale('log')
-        ax.set_xlabel("no. of Richardson Lucy iterations")
-        ax.set_ylabel("sqrt(sum( (iter_n+1 - iter_n)**2 ) )")
-        ax.legend(loc="best")
+        # #STOPPING CRITERIA - stupid for now, do later
+        # ax = fig.add_subplot(2, 2, 4)
+        # ax.title.set_text("change in unfolding")
+        # x = np.linspace(1,args["iterations"], num=args["iterations"])
+        # ax.plot(x,diff1, color='r', label='change between iterations')
+        # ax.plot(x,diff2, color='blue', label='diff between re-convoluted and raw data')
+        # # ax.set_xlim(0,30)
+        # # ax.set_ylim(0,30)
+        # # ax.set_xlim(lower_x,upper_x)
+        # # ax.set_ylim(lower_y,upper_y)
+        # ax.grid()
+        # ax.set_yscale('log')
+        # ax.set_xlabel("no. of Richardson Lucy iterations")
+        # ax.set_ylabel("sqrt(sum( (iter_n+1 - iter_n)**2 ) )")
+        # ax.legend(loc="best")
+
+
+        #FITTED 2D-ELLIPTICAL SUPERGAUSSIAN as contour plot
+        ax = fig.add_subplot(2, 2, 4, projection='3d')
+        ax.title.set_text("fitted data")
+        ax.view_init(elev=45, azim=-45, roll=0)
+        x, y = np.meshgrid(np.linspace(0,30,np.max(data[0]) + 1), np.linspace(0,30,np.max(data[1]) + 1))#meshgrid with lines to 30cm
+
+        #Test Data for plotting a 2D Super Gaus
+        # true_params = [1, 16, 16, 1.0, 1.0, 5]  # A, x0, y0, sigma_x, sigma_y, n
+        # Z_true = super_gaussian_2d((x, y), *true_params).reshape(np.max(data[1]) + 1, np.max(data[1]) + 1)
+        # ax.contour(x, y, Z_true, levels=300, axlim_clip=True)
+        # ax.contourf(x, y, Z_true, zdir='x', offset=lower_x, levels=300, cmap='rainbow', axlim_clip=True)
+        # ax.contourf(x, y, Z_true, zdir='y', offset=upper_y, levels=300, cmap='rainbow', axlim_clip=True)
+
+        popt, pcov = curve_fit(
+                                super_gaussian_2d
+                                ,(x, y)
+                                ,result.ravel()
+                                ,p0=[1, 16, 16, 1.0, 1.0, 15,0]         # initial_guess
+                                ,maxfev=99999999
+                                ,bounds=(
+                                        [0, 0, 0, 0.1, 0.1, 1,0],  # lower bounds
+                                        [1, 30, 30, 500, 500, 100,np.inf]    # upper bounds
+                                        )
+                                )
+        
+        # Extract fitted parameters
+        # A_fit, x0_fit, y0_fit, sigma_x_fit, sigma_y_fit, n_fit, offset_fit = popt
+        A_fit, x0_fit, y0_fit, sigma_x_fit, sigma_y_fit, n_fit, offset_fit = popt
+        print("Fitted parameters for Supergaussian:")
+        print(f"A={A_fit:.3f}, x0={x0_fit:.3f}, y0={y0_fit:.3f}, "
+        f"sigma_x={sigma_x_fit:.3f}, sigma_y={sigma_y_fit:.3f}, "
+        f"n={n_fit:.3f}, offset={offset_fit:.3f}")
+
+        Z_true = super_gaussian_2d((x, y), A_fit, x0_fit, y0_fit, sigma_x_fit, sigma_y_fit, n_fit, offset_fit).reshape(np.max(data[1]) + 1, np.max(data[1]) + 1)
+        ax.contour(x, y, Z_true, levels=300, axlim_clip=True)
+        ax.contourf(x, y, Z_true, zdir='x', offset=lower_x, levels=300, cmap='rainbow', axlim_clip=True)
+        ax.contourf(x, y, Z_true, zdir='y', offset=upper_y, levels=300, cmap='rainbow', axlim_clip=True)
+
+        ax.set_zlim(0,1.1)
+        ax.set_xlim(lower_x,upper_x)
+        ax.set_ylim(lower_y,upper_y)
+        ax.set_xlabel("x / cm")
+        ax.set_ylabel("y / cm")
+        ax.set_zlabel("normalised intensity")
 
         plt.show()
-        # print(np.max(data[0]))
-        # print(np.max(data[1]))
+
     # data_loading.csv_print(out_file, resul)
 
 
