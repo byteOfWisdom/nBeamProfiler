d28d3972eb340a8b84eac4b674034ce82e563a76
Leoni <leoni.dessau@mailbox.org>
2026-02-01 21:58:30 +0100

    timing pulses on both pins

--- timing_pulses/timing_pulses.ino
+++ timing_pulses/timing_pulses.ino
@@ -2,7 +2,8 @@
 #define SET_PIN_HIGH(port, pin) (PORT ## port |= (1 << pin))
 #define SET_PIN_LOW(port, pin) ((PORT ## port) &= ~(1 << (pin)))
 
-#define INT_PIN 2
+#define INT_PIN_A 2
+#define INT_PIN_B 3
 
 inline void rect_pulse(void) {
   SET_PIN_HIGH(B, 0);  
@@ -12,8 +13,10 @@ inline void rect_pulse(void) {
 
 void setup() {
   SET_PIN_MODE_OUTPUT(B, 0);
-  pinMode(INT_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(INT_PIN), rect_pulse, CHANGE);
+  pinMode(INT_PIN_A, INPUT);
+  pinMode(INT_PIN_A, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN_A), rect_pulse, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN_B), rect_pulse, CHANGE);
 }
 
 void loop() {
