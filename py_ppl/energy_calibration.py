d84278d0ad224f19c2050aa98aed5c0fca2f3b42
Leoni <leoni.dessau@mailbox.org>
2026-07-17 13:39:46 +0200

    asdf

--- py_ppl/energy_calibration.py
+++ py_ppl/energy_calibration.py
@@ -5,6 +5,12 @@ from matplotlib import pyplot as plt
 from sys import argv
 
 
+class config:
+    filter_len = 25
+    smoothing_kernel = np.ones(25) / 2
+    long_bins = 1500
+
+
 def edge_fn(x, a, x0, sigma, b, c):
     return a * sp.special.erfc((x - x0) / (np.sqrt(2) * sigma)) + b * x + c
 
@@ -12,10 +18,16 @@ def edge_fn(x, a, x0, sigma, b, c):
 def local_mins(data, bins):
     data = np.log(data)
     data[data < 0] = 0
-    data = sp.signal.medfilt(data, 25)
-    data = np.convolve(data, np.ones(25), "same")
+    data = sp.signal.medfilt(data, config.filter_len)
+    data = np.convolve(data, config.smoothing_kernel, "same")
     diff = np.gradient(data, bins)
+    diff *= -1
+    diff[diff < 0] = 0
+    diff = np.convolve(diff, config.smoothing_kernel, "same")
+    peaks, _ = sp.signal.find_peaks(diff, width=10, prominence=0.1)
+    print(peaks)
     plt.plot(bins, diff)
+    plt.scatter(bins[peaks], diff[peaks])
     # plt.plot(bins, data)
     plt.show()
 
@@ -24,7 +36,7 @@ def main():
     fname = argv[1]
     data = data_loading.load_dataset(fname)
     print(data.long)
-    hist, bins = np.histogram(data.long, bins=1500)
+    hist, bins = np.histogram(data.long, bins=config.long_bins)
     bin_centers = 0.5 * (bins[:-1] + bins[1:])
     x0_guesses = local_mins(hist, bin_centers)
     plt.plot(bin_centers, hist)
