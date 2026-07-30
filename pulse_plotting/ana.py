0cb312f3ac1dea811eaa832a091942bb468fde55
Leoni <leoni.dessau@mailbox.org>
2026-07-30 19:21:27 +0200

    asdf

--- pulse_plotting/ana.py
+++ pulse_plotting/ana.py
@@ -89,80 +89,124 @@ def psd(time, voltage):
     return y, long_value
 
 
-if __name__ == "__main__":
+def pre_process(time, voltage):
+    voltage -= np.average(voltage[:50])
+    voltage = norm(voltage)
+    if pile_up_reject(time, voltage):
+        return True, time, voltage
+    time, voltage = retrigger(time, voltage)
+    time, voltage = trim(time, voltage)
+    return False, time, voltage
+    
+
+def parse_args():
     plot_hist = False
     only_avg = False
     if len(argv) > 4 and argv[4] == "hist":
         plot_hist = True
     if len(argv) > 4 and argv[4] == "avg":
         only_avg = True
+    return plot_hist, only_avg
 
-    start_file, stop_file = int(argv[1]), int(argv[2])
-    file_count = start_file - stop_file + 1
-    ys = np.zeros(file_count)
-    long = np.zeros(file_count)
-    print(f"loading {len(ys)} files")
-    rejected = 0
+
+class analysis:
     n_gamma_cutoff = 0.45 # TODO
-    progress_bar = pbar(stop_file - start_file, "", 40)
-    cache = sciebo_fetch.open(argv[3])
-
-    delim = "\t"
     t, u = None, None
-    try:
-        t, u = np.genfromtxt(cache.np_loadable(cache.ls()[start_file]), delimiter=delim, unpack=True, skip_header=5)
-    except ValueError:
-        delim = ","
-        t, u = np.genfromtxt(cache.np_loadable(cache.ls()[start_file]), delimiter=delim, unpack=True, skip_header=5)
-    t, u = retrigger(t, u)
-    t, u = trim(t, u)
-    avg_pulse_gamma = np.zeros(len(t))
-    avg_pulse_neutron = np.zeros(len(t))
-    refrence_timescale = t
+    plot_all = False
 
+    def __init__(self, filename, start_file, stop_file, show_pbar=True):
+        self.start_file, self.stop_file = start_file, stop_file
+        self.file_count = stop_file - start_file + 1
+        self.ys = np.zeros(self.file_count)
+        self.long = np.zeros(self.file_count)
+        print(f"loading {len(self.ys)} files")
 
-    gamma_count, neutron_count = 0, 0
-    i = -1
-    for fname in cache.ls()[start_file:stop_file]:
-        i += 1
-        progress_bar.next()
+        # init various counters
+        self.rejected = 0
+        if show_pbar:
+            self.progress_bar = pbar(self.file_count, "", 40)
+            self.has_pbar = True
+        else:
+            self.has_pbar = False
 
-        t, u = np.genfromtxt(cache.np_loadable(fname), delimiter=delim, unpack=True, skip_header=5)
-        u -= np.average(u[:50])
-        u = norm(u)
-        if pile_up_reject(t, u):
-            rejected += 1
-            continue
+        self.cache = sciebo_fetch.open(filename)
+        self.file_names = self.cache.ls()
+        self.delimiter = "\t"
+        self.current_filenum = -1
+        self.gamma_count, self.neutron_count = 0, 0
+
+    def init_reader(self):
+        t, u = None, None
+        try:
+            t, u = np.genfromtxt(self.cache.np_loadable(self.cache.ls()[self.start_file]), delimiter=self.delimiter, unpack=True, skip_header=5)
+        except ValueError:
+            self.delimiter = ","
+            t, u = np.genfromtxt(self.cache.np_loadable(self.cache.ls()[self.start_file]), delimiter=self.delimiter, unpack=True, skip_header=5)
         t, u = retrigger(t, u)
         t, u = trim(t, u)
-        y, long_sample = psd(t, u)
-        ys[i] = y
-        long[i] = long_sample
-        if not plot_hist and not only_avg:
-            plt.plot(t, u)
-        if only_avg:
-            if y > n_gamma_cutoff:
-                neutron_count += 1
-                avg_pulse_neutron += (u / min(u))
-            else:
-                gamma_count += 1
-                avg_pulse_gamma += (u / min(u))
+        self.avg_pulse_gamma = np.zeros(len(t))
+        self.avg_pulse_neutron = np.zeros(len(t))
+        self.refrence_timescale = t
 
+    def load_next(self):
+        self.current_filenum += 1
+        file = self.file_names[self.current_filenum + self.start_file]
+        self.t, self.u = np.genfromtxt(self.cache.np_loadable(file), delimiter=self.delimiter, unpack=True, skip_header=5)
+
+        if self.has_pbar:
+            self.progress_bar.next()
+        return self.current_filenum < self.file_count
+
+    def pre_process(self):
+        reject, temp_t, temp_u = pre_process(self.t, self.u)
+        if reject:
+            self.rejected += 1
+            return False
+        self.t, self.u = temp_t, temp_u
+        return True
+
+    def psd(self):
+        self.y_current, self.long_current = psd(self.t, self.u)
+        self.ys[self.current_filenum] = self.y_current
+        self.long[self.current_filenum] = self.long_current
+        if self.y_current > self.n_gamma_cutoff:
+            self.neutron_count += 1
+            self.avg_pulse_neutron += (self.u / min(self.u))
+        else:
+            self.gamma_count += 1
+            self.avg_pulse_gamma += (self.u / min(self.u))
+
+    def process(self):
+        if not self.pre_process():
+            return
+        self.psd()
+        if self.plot_all:
+            plt.plot(self.t, self.u)
             
+    def run(self):
+        self.init_reader()
+        while(self.load_next()):
+            self.process()
 
-    print(f"rejected {rejected} for pile ups")
+
+if __name__ == "__main__":
+    plot_hist, only_avg = parse_args()
+    filename = argv[3]
+    start_file, stop_file = int(argv[1]), int(argv[2])
+    ana = analysis(filename, start_file, stop_file)
+    ana.plot_all = not (plot_hist or only_avg)
+    ana.run()
+
+    print(f"rejected {ana.rejected} for pile ups")
 
 
     
     if plot_hist:
-        print(ys)
-        plt.hist2d(np.abs(long[ys != 0]), ys[ys != 0], (75, 75), range=[[0, 4e-8], [0.2, 0.75]])
-        # plt.hist(ys[ys != 0], 50, (0, 1))
-        # plt.hist2d(np.abs(long[ys != 0]), ys[ys != 0], (75, 75))
-        # plt.hist(ys[ys != 0], 100, (0, 1) )
+        print(ana.ys)
+        plt.hist2d(np.abs(ana.long[ana.ys != 0]), ana.ys[ana.ys != 0], (75, 75), range=[[0, 4e-8], [0.2, 0.75]])
     elif only_avg:
-        n_time, n_amp = retrigger(refrence_timescale, avg_pulse_neutron / neutron_count)
-        gamma_time, gamma_amp = retrigger(refrence_timescale, avg_pulse_gamma / gamma_count)
+        n_time, n_amp = retrigger(ana.refrence_timescale, ana.avg_pulse_neutron / ana.neutron_count)
+        gamma_time, gamma_amp = retrigger(ana.refrence_timescale, ana.avg_pulse_gamma / ana.gamma_count)
         n_res, _ = curve_fit(pulse_function, n_time, n_amp, [max(n_amp), 5e-9, n_time[np.argmax(n_amp)], 1e-9])
         gamma_res, _ = curve_fit(pulse_function, gamma_time, gamma_amp, [max(gamma_amp), 5e-9, gamma_time[np.argmax(gamma_amp)], 1e-9])
         print(n_res)
