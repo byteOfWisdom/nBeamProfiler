fa5bdf7019c2514b7ec97c4bcfd3209feb9f53ba
Leoni <leoni.dessau@mailbox.org>
2026-09-16 19:14:58 +0200

    adsf

--- py_ppl/psd_plot.py
+++ py_ppl/psd_plot.py
@@ -4,11 +4,7 @@ from sys import argv
 import sciebo_fetch
 import numpy as np
 import scipy
-
-
-# a = 2.85880836e+08 
-# b = 3.23529442e-09
-# c = -9.10068348e-01
+from types import NoneType
 
 
 def energy_to_ch(x):
@@ -25,14 +21,14 @@ def ch_to_energy(x):
     return np.real((a * b * scipy.special.lambertw(c * np.exp(c / (a * b) + x / a) / (a * b)) - c) / (b * c))
 
 
-print(energy_to_ch(4.2))
-
 if __name__ == "__main__":
     if len(argv) < 3:
-        print('["local" | URL of sciebo] [filename] [OPTONAL ARGS ...]')
+        print('python psd_plot.py ["local" | URL of sciebo] [filename | "ls"] [OPTONAL ARGS ...]')
+        print(' [filename] may be replaced by "ls" to list and interactively pick a file from a sciebo archive')
         print('\toptional args:')
         print(' --bins [number] : sets both x and y direction bin counts to that number')
-        print(' --Ecal [number] [number] : slope and offset for a linear relation from channels to energies. slope as eV / Channel')
+        print(' --cut [number] : display the neutron / gamma discrimination line at given value')
+        exit(0)
 
     source = argv[1]
     name = argv[2]
@@ -40,18 +36,18 @@ if __name__ == "__main__":
     if "--bins" in argv:
         bin_count = int(argv[1 + argv.index("--bins")])
 
-    has_ecal = False
-    ecal = None
-    if "--Ecal" in argv:
-        has_ecal = True
-        slope = float(argv[1 + argv.index("--Ecal")])
-        offset = float(argv[2 + argv.index("--Ecal")])
-        def linear_func(ch): return ch * slope + offset
-        ecal = linear_func
+    n_gamma_cut = None
+    if "--cut" in argv:
+        n_gamma_cut = float(argv[1 + argv.index("--cut")])
 
     load_handle = None
     if source != "local":
         data_store = sciebo_fetch.fetch(source, True)
+        if name == "ls":
+            files = data_store.ls()
+            for i in range(len(files)):
+                print(f"[{i}] {files[i]}")
+            name = files[int(input("select file by number: "))]
         load_handle = data_store.np_loadable(name)
     else:
         load_handle = name
@@ -62,10 +58,16 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots()
 
-    ax.hist2d(dataset.long, dataset.y(), bins=(bin_count, bin_count), range=((0, max(dataset.long)), (0, max(dataset.y()[dataset.y() < 1]))))
+    particle_hist = ax.hist2d(dataset.long, dataset.y(), bins=(bin_count, bin_count), range=((0, max(dataset.long)), (0, max(dataset.y()[dataset.y() < 1]))), cmap="plasma")
     ax.set_xlabel("long / channel")
     ax.set_ylabel("$Q$")
     energy_axis = ax.secondary_xaxis("top", functions=(ch_to_energy, energy_to_ch))
     energy_axis.set_xlabel("energy / MeV")
+    fig.colorbar(particle_hist[3])
+
+    if not isinstance(n_gamma_cut, NoneType):
+        ax.axhline(n_gamma_cut, linestyle="dashed", color="white")
+        ax.text(max(dataset.long) - 10000, n_gamma_cut + 0.02, "neutrons", color="white")
+        ax.text(max(dataset.long) - 10000, n_gamma_cut - 0.03, "gammas", color="white")
 
     plt.show()
